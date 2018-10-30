109d904aa0b750153b3d16bd870a041f3cbf26a9
Maciej <maciekcwiakowski@gmail.com>
2018-10-30 21:54:42 +0100

    Added option to display pictures

---
+++ Gallery/app/src/main/java/com/cwiakowski/pam/gallery/activities/SlideShowActivity.java
@@ -0,0 +1,16 @@
+package com.cwiakowski.pam.gallery.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cwiakowski.pam.gallery.R;
+
+public class SlideShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+
+    }
+}
