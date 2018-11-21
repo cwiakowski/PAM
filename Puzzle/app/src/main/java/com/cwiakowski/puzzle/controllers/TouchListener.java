fb805730bacae60410f861c79b9985b6a95f8985
Maciej <maciekcwiakowski@gmail.com>
2018-11-22 00:14:48 +0100

    Bugfixes

--- Puzzle/app/src/main/java/com/cwiakowski/puzzle/controllers/TouchListener.java
+++ Puzzle/app/src/main/java/com/cwiakowski/puzzle/controllers/TouchListener.java
@@ -4,6 +4,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.cwiakowski.puzzle.R;
 import com.cwiakowski.puzzle.entities.PuzzlePiece;
 import com.cwiakowski.puzzle.views.PuzzleActivity;
 
@@ -22,13 +23,14 @@ public class TouchListener implements View.OnTouchListener {
     public boolean onTouch(View view, MotionEvent motionEvent) {
         float x = motionEvent.getRawX();
         float y = motionEvent.getRawY();
-        final double maxOffset = 40;
+        final double maxOffset = 45;
 
         PuzzlePiece piece = (PuzzlePiece) view;
         if (!piece.canMove) {
             return true;
         }
 
+        RelativeLayout layout = view.findViewById(R.id.layout);
         RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
@@ -37,8 +39,17 @@ public class TouchListener implements View.OnTouchListener {
                 piece.bringToFront();
                 break;
             case MotionEvent.ACTION_MOVE:
+
                 lParams.leftMargin = (int) (x - xDelta);
                 lParams.topMargin = (int) (y - yDelta);
+                if (lParams.leftMargin <= 0)
+                    lParams.leftMargin = 0;
+                if(lParams.topMargin <= 0)
+                    lParams.topMargin = 0;
+                if (lParams.leftMargin + piece.width > 1080)
+                    lParams.leftMargin = 1080 - piece.width;
+                if (lParams.topMargin + piece.height >1572)
+                    lParams.topMargin = 1572 - piece.height;
                 view.setLayoutParams(lParams);
                 break;
             case MotionEvent.ACTION_UP:
