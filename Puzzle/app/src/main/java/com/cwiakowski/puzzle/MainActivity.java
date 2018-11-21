5508d8d0f7e34b943528827cce5edc774dc8a140
Maciej <maciekcwiakowski@gmail.com>
2018-11-21 18:15:28 +0100

    Added displaying and splitting pic methods

--- Puzzle/app/src/main/java/com/cwiakowski/puzzle/MainActivity.java
+++ Puzzle/app/src/main/java/com/cwiakowski/puzzle/MainActivity.java
@@ -1,13 +1,110 @@
 package com.cwiakowski.puzzle;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Bitmap> pieces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout layout = findViewById(R.id.layout);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        imageView.post(new Runnable() {
+            @Override
+            public void run() {
+                pieces = splitImage();
+                for(Bitmap piece: pieces) {
+                    ImageView iv = new ImageView(getApplicationContext());
+                    iv.setImageBitmap(piece);
+                    layout.addView(iv);
+                }
+            }
+        });
     }
+
+    private ArrayList<Bitmap> splitImage() {
+        int piecesNumber = 12;
+        int rows = 4;
+        int cols = 3;
+
+        ImageView imageView = findViewById(R.id.imageView);
+        ArrayList<Bitmap> pieces = new ArrayList<>(piecesNumber);
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        int[] dimensions = getBitmapPositionInsideImageView(imageView);
+        int scaledBitmapLeft = dimensions[0];
+        int scaledBitmapTop = dimensions[1];
+        int scaledBitmapWidth = dimensions[2];
+        int scaledBitmapHeight = dimensions[3];
+
+        int croppedImageWidth = scaledBitmapWidth - 2*abs(scaledBitmapLeft);
+        int croppedImageHeight = scaledBitmapHeight - 2*abs(scaledBitmapTop);
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledBitmapWidth, scaledBitmapHeight, true);
+        Bitmap croppedBitmap = Bitmap.createBitmap(scaledBitmap, abs(scaledBitmapLeft), abs(scaledBitmapTop), croppedImageWidth, croppedImageHeight);
+
+        int pieceWidth = croppedImageWidth/cols;
+        int pieceHeight = croppedImageHeight/rows;
+
+        int yCord = 0;
+        for (int row = 0; row < rows; row++) {
+            int xCord = 0;
+            for (int col = 0; col < cols; col++) {
+                pieces.add(Bitmap.createBitmap(croppedBitmap, xCord, yCord, pieceWidth, pieceHeight));
+                xCord += pieceWidth;
+            }
+            yCord += pieceHeight;
+        }
+        return pieces;
+    }
+
+    private int[] getBitmapPositionInsideImageView(ImageView imageView) {
+        int[] ret = new int[4];
+        if (imageView == null || imageView.getDrawable() == null)
+            return ret;
+
+        float[] f = new float[9];
+        imageView.getImageMatrix().getValues(f);
+
+        final float scaleX = f[Matrix.MSCALE_X];
+        final float scaleY = f[Matrix.MSCALE_Y];
+
+        final Drawable d = imageView.getDrawable();
+        final int origW = d.getIntrinsicWidth();
+        final int origH = d.getIntrinsicHeight();
+
+        final int actW = Math.round(origW * scaleX);
+        final int actH = Math.round(origH * scaleY);
+
+        ret[2] = actW;
+        ret[3] = actH;
+
+        int imgViewW = imageView.getWidth();
+        int imgViewH = imageView.getHeight();
+
+        int top = (int) (imgViewH - actH)/2;
+        int left = (int) (imgViewW - actW/2);
+
+        ret[0] = left;
+        ret[1] = top;
+        return ret;
+    }
+
 }
